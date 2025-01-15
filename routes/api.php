6d2ed691212e1f21be583057569633d441339863
auxsenadesarrollo <auxsenadesarrollo@consuerte.com.co>
2025-01-15 13:43:07 -0500

    Se crean las rutas en routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,47 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//Rutas BodegaController
+
 Route::controller(BodegaController::class)->group(function(){
     Route::get('bodega/datos','getData');
+    Route::save('bodega/guardar','save');
+    Route::put('bodega/actualizar','update');
+    Route::delete('bodega/borrer','destroy');
+});
+
+//Rutas InventarioController
+
+Route::controller(InventarioController::class)->group(function(){
+    Route::get('inventario/datos','getData');
+    Route::save('inventario/guardar','save');
+    Route::put('inventario/actualizar','update');
+    Route::delete('inventario/borrer','destroy');
+});
+
+//Rutas MarcaController
+
+Route::controller(MarcaController::class)->group(function(){
+    Route::get('marca/datos','getData');
+    Route::save('marca/guardar','save');
+    Route::put('marca/actualizar','update');
+    Route::delete('marca/borrer','destroy');
+});
+
+//Rutas ProductoController
+
+Route::controller(ProductoController::class)->group(function(){
+    Route::get('producto/datos','getData');
+    Route::save('producto/guardar','save');
+    Route::put('producto/actualizar','update');
+    Route::delete('producto/borrer','destroy');
+});
+
+//Rutas ProveedorController
+
+Route::controller(BodegaController::class)->group(function(){
+    Route::get('bodega/datos','getData');
+    Route::save('bodega/guardar','save');
+    Route::put('bodega/actualizar','update');
+    Route::delete('bodega/borrer','destroy');
 });
